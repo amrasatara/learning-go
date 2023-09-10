38ceb181911fc771614d692df9b080acc20d32d8
amrasatara <amra.satara@gmail.com>
2023-09-10 19:56:10 +0200

    d

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amrasatara/learning-go
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello world")
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
