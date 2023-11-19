d6ee5351a58c2727a29dd8ec17e62fc36b9b2fb4
amrasatara <amra.satara@gmail.com>
2023-11-19 16:44:49 +0100

    2016 day3

--- aoc2016/day03.go
+++ aoc2016/day03.go
@@ -1,15 +1,71 @@
 package aoc2016
 
-import "github.com/amra.satara/learning-go/fileparsers"
+import (
+	"fmt"
+
+	"github.com/amra.satara/learning-go/fileparsers"
+)
 
 func Day03Part1() int {
 	input := fileparsers.ReadLines("inputs2016\\day3.txt")
+	counter := 0
+	for _, line := range input {
+		t := ParseLine(line)
+		if t.IsValid() {
+			counter++
+		}
+	}
 
-	return len(input)
+	return counter
 }
 
 func Day03Part2() int {
 	input := fileparsers.ReadLines("inputs2016\\day3.txt")
 
-	return len(input)
+	list := make([]Triangle, 0)
+	for i := 2; i < len(input); i += 3 {
+		var t1, t2, t3 Triangle
+		fmt.Sscanf(input[i-2], "%5d%5d%5d", &t1.A, &t2.A, &t3.A)
+		fmt.Sscanf(input[i-1], "%5d%5d%5d", &t1.B, &t2.B, &t3.B)
+		fmt.Sscanf(input[i], "%5d%5d%5d", &t1.C, &t2.C, &t3.C)
+		list = append(list, t1)
+		list = append(list, t2)
+		list = append(list, t3)
+	}
+
+	return CountValid(list)
+}
+
+func ParseLine(line string) Triangle {
+	var s1, s2, s3 int
+	fmt.Sscanf(line, "%5d%5d%5d", &s1, &s2, &s3)
+	return Triangle{
+		A: s1,
+		B: s2,
+		C: s3,
+	}
+}
+
+type Triangle struct {
+	A int
+	B int
+	C int
+}
+
+func (t *Triangle) IsValid() bool {
+	return t.A+t.B > t.C &&
+		t.B+t.C > t.A &&
+		t.C+t.A > t.B
+}
+
+func CountValid(list []Triangle) int {
+
+	counter := 0
+	for _, t := range list {
+		if t.IsValid() {
+			counter++
+		}
+
+	}
+	return counter
 }
--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 
 func main() {
 
-	// fmt.Println("Day 1 part 1: ", aoc2016.Day01Part1())
-	// fmt.Println("Day 1 part 2: ", aoc2016.Day01Part2())
-	fmt.Println("Day 2 part 1: ", aoc2016.Day02Part1())
-	fmt.Println("Day 2 part 2: ", aoc2016.Day02Part2())
+	// fmt.Println("Day 1 part 1: ", aoc2016.Day01Part1()) //332
+	// fmt.Println("Day 1 part 2: ", aoc2016.Day01Part2()) //166
+	// fmt.Println("Day 2 part 1: ", aoc2016.Day02Part1()) //38961
+	// fmt.Println("Day 2 part 2: ", aoc2016.Day02Part2()) //46C92
+	fmt.Println("Day 3 part 1: ", aoc2016.Day03Part1()) //1050
+	fmt.Println("Day 3 part 2: ", aoc2016.Day03Part2()) //46C92
 
 }
