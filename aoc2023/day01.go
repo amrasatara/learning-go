2918d3d54cbe04127dc21533f75f490610889828
amrasatara <amra.satara@gmail.com>
2023-12-01 19:17:21 +0100

    2023 day1

--- aoc2023/day01.go
+++ aoc2023/day01.go
@@ -2,66 +2,126 @@ package aoc2023
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/amra.satara/learning-go/fileparsers"
-	"github.com/amra.satara/learning-go/positions"
 )
 
 func Day01Part1() int {
 	input := fileparsers.ReadLines("inputs2023\\day1.txt")
-	partsI := fileparsers.ParseToStringObj(input[0], ", ")
-	parts := parseInput(partsI)
 
-	position := positions.Position{
-		X:    0,
-		Y:    0,
-		Side: positions.Side{Name: "N"},
+	sum := 0
+	for _, v := range input {
+		x := parseNumber(v)
+		sum += x
 	}
-	for _, l := range parts {
-		position.Move(l.Direction, l.Steps, nil)
-	}
-	return position.CalculateFrom0() //332
+	return sum
 }
 
 func Day01Part2() int {
 	input := fileparsers.ReadLines("inputs2023\\day1.txt")
-	partsI := fileparsers.ParseToStringObj(input[0], ", ")
-	parts := parseInput(partsI)
 
-	position := positions.Position{
-		X:    0,
-		Y:    0,
-		Side: positions.Side{Name: "N"},
+	sum := 0
+	for _, v := range input {
+		x := parseNumberAndLetters(v)
+		sum += x
 	}
-	visited := make(map[string]bool)
-	for _, l := range parts {
-		position.Move(l.Direction, l.Steps, visited)
-	}
-	return position.CalculateFrom0() //332
+	return sum
 }
 
-func parseInput(lines []string) []InstDay1 {
-	result := make([]InstDay1, len(lines))
+func parseNumber(input string) int {
+	first := -1
+	last := 0
 
-	for index, line := range lines {
-		result[index] = parseItem(line)
+	for _, v := range input {
+		if v >= rune('0') && v <= rune('9') {
+			x, _ := strconv.Atoi(string(v))
+			if first == -1 {
+				first = x
+			}
+			last = x
+		}
 	}
 
+	if first == -1 {
+		first = 0
+	}
+
+	n := first*10 + last
+	fmt.Println(input, n)
+	return n
+}
+func parseNumberAndLetters(input string) int {
+	result := 0
+	n := findFirstNumber(input) + string(findLastNumber(input))
+	fmt.Sscanf(n, "%d", &result)
 	return result
 }
 
-func parseItem(line string) InstDay1 {
-	var s1 string
-	var s2 int
-	fmt.Sscanf(line, "%1s%d", &s1, &s2)
+func findFirstNumber(input string) string {
+	min := len(input)
+	minValue := ""
 
-	return InstDay1{
-		Direction: s1,
-		Steps:     s2,
+	for _, v := range listForReplace {
+		index := strings.Index(input, v.From)
+		if index != -1 && index < min {
+			min = index
+			minValue = v.To[0:1]
+		}
 	}
+	return minValue
 }
 
-type InstDay1 struct {
-	Direction string
-	Steps     int
+func findLastNumber(input string) string {
+
+	max := -1
+	maxValue := ""
+
+	for _, v := range listForReplace {
+
+		index := strings.LastIndex(input, v.From)
+		if index != -1 && index > max {
+			max = index
+			length := len(v.To)
+			maxValue = v.To[length-1 : length]
+		}
+	}
+	return maxValue
+}
+
+type Dataday1 struct {
+	From string
+	To   string
+}
+
+var listForReplace = []Dataday1{
+	{From: "ten", To: "10"},
+	{From: "eleven", To: "11"},
+	{From: "twelve", To: "12"},
+	{From: "thirteen", To: "13"},
+	{From: "fourteen", To: "14"},
+	{From: "fifteen", To: "15"},
+	{From: "sixteen", To: "16"},
+	{From: "seventeen", To: "17"},
+	{From: "eighteen", To: "18"},
+	{From: "nineteen", To: "19"},
+	{From: "one", To: "1"},
+	{From: "two", To: "2"},
+	{From: "three", To: "3"},
+	{From: "four", To: "4"},
+	{From: "five", To: "5"},
+	{From: "six", To: "6"},
+	{From: "seven", To: "7"},
+	{From: "eight", To: "8"},
+	{From: "nine", To: "9"},
+	{From: "1", To: "1"},
+	{From: "2", To: "2"},
+	{From: "3", To: "3"},
+	{From: "4", To: "4"},
+	{From: "5", To: "5"},
+	{From: "6", To: "6"},
+	{From: "7", To: "7"},
+	{From: "8", To: "8"},
+	{From: "9", To: "9"},
 }
--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/amra.satara/learning-go/aoc2016"
+	"github.com/amra.satara/learning-go/aoc2023"
 )
 
 func main() {
 
-	// fmt.Println("Day 1 part 1: ", aoc2016.Day01Part1()) //332
+	fmt.Println("Day 1 part 1: ", aoc2023.Day01Part1()) //53386
+	fmt.Println("Day 1 part 2: ", aoc2023.Day01Part2()) //53312
 	// fmt.Println("Day 1 part 2: ", aoc2016.Day01Part2()) //166
 	// fmt.Println("Day 2 part 1: ", aoc2016.Day02Part1()) //38961
 	// fmt.Println("Day 2 part 2: ", aoc2016.Day02Part2()) //46C92
-	fmt.Println("Day 3 part 1: ", aoc2016.Day03Part1()) //1050
-	fmt.Println("Day 3 part 2: ", aoc2016.Day03Part2()) //46C92
+	// fmt.Println("Day 3 part 1: ", aoc2016.Day03Part1()) //1050
+	// fmt.Println("Day 3 part 2: ", aoc2016.Day03Part2()) //2778
+	// fmt.Println("Day 4 part 1: ", aoc2016.Day04Part1()) //158835
+	// fmt.Println("Day 4 part 2: ", aoc2016.Day04Part2()) //158835
 
 }
