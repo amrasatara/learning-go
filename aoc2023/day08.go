cbb34221cecd0d9fb9632269ae465721a1b518e1
amrasatara <amra.satara@gmail.com>
2023-12-09 23:11:32 +0100

    2023 day8

--- aoc2023/day08.go
+++ aoc2023/day08.go
@@ -1,15 +1,116 @@
 package aoc2023
 
-import "github.com/amra.satara/learning-go/fileparsers"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amra.satara/learning-go/fileparsers"
+)
+
+var directions []rune
+var instructions []*InstrDay8
 
 func Day08Part1() int {
 	input := fileparsers.ReadLines("inputs2023\\day8.txt")
 
-	return len(input)
+	MakeDirections(input[0])
+	MakeInstructions(input[2:])
+
+	return FindSteps(GetInstr("AAA"), "ZZZ")
+}
+
+func FindSteps(next *InstrDay8, endsWith string) int {
+	for i := 0; ; i++ {
+		d := NextDirection(i)
+		if d == 'R' {
+			next = next.Right
+		} else {
+			next = next.Left
+		}
+		if len(next.Name) > len(strings.TrimSuffix(next.Name, endsWith)) {
+			return i + 1
+		}
+	}
 }
 
 func Day08Part2() int {
 	input := fileparsers.ReadLines("inputs2023\\day8.txt")
 
-	return len(input)
+	MakeDirections(input[0])
+	MakeInstructions(input[2:])
+
+	list := make([]int, 0)
+
+	for _, instruction := range instructions {
+
+		if instruction.Name[2] == 'A' {
+			list = append(list, FindSteps(instruction, "Z"))
+		}
+
+	}
+
+	result := list[0]
+	for i := 1; i < len(list); i++ {
+		result = lcm(result, list[i])
+
+	}
+	return result
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func MakeInstructions(list []string) {
+
+	instructions = make([]*InstrDay8, 0)
+
+	for _, v := range list {
+		var s1, s2, s3 string
+
+		fmt.Sscanf(v, "%3s = (%3s, %3s)", &s1, &s2, &s3)
+		i1 := GetInstr(s1)
+		i2 := GetInstr(s2)
+		i3 := GetInstr(s3)
+
+		i1.Left = i2
+		i1.Right = i3
+	}
+}
+
+func GetInstr(s1 string) *InstrDay8 {
+	for _, r := range instructions {
+		if r.Name == s1 {
+			return r
+		}
+	}
+	r2 := InstrDay8{
+		Name: s1,
+	}
+	instructions = append(instructions, &r2)
+
+	return &r2
+}
+
+type InstrDay8 struct {
+	Name  string
+	Left  *InstrDay8
+	Right *InstrDay8
+}
+
+func MakeDirections(input string) {
+	directions = []rune(input)
+}
+
+func NextDirection(step int) rune {
+	max := len(directions)
+
+	return directions[step%max]
 }
--- main.go
+++ main.go
@@ -8,19 +8,22 @@ import (
 
 func main() {
 
-	fmt.Println("Day 1 part 1: ", aoc2023.Day01Part1()) //53386
-	fmt.Println("Day 1 part 2: ", aoc2023.Day01Part2()) //53312
-	fmt.Println("Day 2 part 1: ", aoc2023.Day02Part1()) //2204
-	fmt.Println("Day 2 part 2: ", aoc2023.Day02Part2()) //71036
-	fmt.Println("Day 3 part 1: ", aoc2023.Day03Part1()) //512794
-	fmt.Println("Day 3 part 2: ", aoc2023.Day03Part2()) //67779080
-	fmt.Println("Day 4 part 1: ", aoc2023.Day04Part1()) //23235
-	fmt.Println("Day 4 part 2: ", aoc2023.Day04Part2()) //5920640
-	fmt.Println("Day 5 part 1: ", aoc2023.Day05Part1()) //486613012
-	fmt.Println("Day 5 part 2: ", aoc2023.Day05Part2()) //56931769
-	fmt.Println("Day 6 part 1: ", aoc2023.Day06Part1()) //2612736
-	fmt.Println("Day 6 part 2: ", aoc2023.Day06Part2()) //29891250
-	fmt.Println("Day 7 part 1: ", aoc2023.Day07Part1()) //248836197
-	fmt.Println("Day 7 part 2: ", aoc2023.Day07Part2()) //251195607
+	// fmt.Println("Day 1 part 1: ", aoc2023.Day01Part1()) //53386
+	// fmt.Println("Day 1 part 2: ", aoc2023.Day01Part2()) //53312
+	// fmt.Println("Day 2 part 1: ", aoc2023.Day02Part1()) //2204
+	// fmt.Println("Day 2 part 2: ", aoc2023.Day02Part2()) //71036
+	// fmt.Println("Day 3 part 1: ", aoc2023.Day03Part1()) //512794
+	// fmt.Println("Day 3 part 2: ", aoc2023.Day03Part2()) //67779080
+	// fmt.Println("Day 4 part 1: ", aoc2023.Day04Part1()) //23235
+	// fmt.Println("Day 4 part 2: ", aoc2023.Day04Part2()) //5920640
+	// fmt.Println("Day 5 part 1: ", aoc2023.Day05Part1()) //486613012
+	// fmt.Println("Day 5 part 2: ", aoc2023.Day05Part2()) //56931769
+	// fmt.Println("Day 6 part 1: ", aoc2023.Day06Part1()) //2612736
+	// fmt.Println("Day 6 part 2: ", aoc2023.Day06Part2()) //29891250
+	// fmt.Println("Day 7 part 1: ", aoc2023.Day07Part1()) //248836197
+	// fmt.Println("Day 7 part 2: ", aoc2023.Day07Part2()) //251195607
+
+	fmt.Println("Day 8 part 1: ", aoc2023.Day08Part1()) //
+	fmt.Println("Day 8 part 2: ", aoc2023.Day08Part2()) //
 
 }
