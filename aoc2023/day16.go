968b852748601b2bdf2f9aa7b74c6b38785874ea
amrasatara <amra.satara@gmail.com>
2023-12-16 23:14:07 +0100

    2023 day16

--- aoc2023/day16.go
+++ aoc2023/day16.go
@@ -1,15 +1,186 @@
 package aoc2023
 
-import "github.com/amra.satara/learning-go/fileparsers"
+import (
+	"strconv"
+
+	"github.com/amra.satara/learning-go/fileparsers"
+)
+
+var dataday16 [][]rune
+var sizeX16, sizeY16 int
+var paths map[string]int
+var uniquepaths map[string]int
 
 func Day16Part1() int {
 	input := fileparsers.ReadLines("inputs2023\\day16.txt")
 
-	return len(input)
+	parseDay16(input)
+	paths = make(map[string]int, 0)
+	uniquepaths = make(map[string]int, 0)
+
+	GoNext('W', 0, 0)
+
+	return len(paths)
+}
+
+func GoNext(from rune, x, y int) {
+	if x < 0 || y < 0 || x >= sizeX16 || y >= sizeY16 {
+		return
+	}
+
+	c := dataday16[y][x]
+	//fmt.Println("c je ", string(c), " na ", x, " ", y)
+	key := string(from) + strconv.Itoa(x) + "-" + strconv.Itoa(y)
+	key2 := strconv.Itoa(x) + "-" + strconv.Itoa(y)
+
+	v, ok1 := paths[key2]
+
+	if ok1 {
+		paths[key2] = v + 1
+	} else {
+		paths[key2] = 1
+	}
+	_, ok := uniquepaths[key]
+	if ok {
+		return
+	} else {
+		uniquepaths[key] = 1
+	}
+
+	if c == '.' || (c == '-' && (from == 'E' || from == 'W')) || (c == '|' && (from == 'N' || from == 'S')) {
+		switch from {
+		case 'E':
+			{
+				GoNext(from, x-1, y)
+			}
+		case 'W':
+			{
+				GoNext(from, x+1, y)
+			}
+		case 'N':
+			{
+				GoNext(from, x, y+1)
+			}
+		case 'S':
+			{
+				GoNext(from, x, y-1)
+			}
+		}
+	} else if c == '\\' {
+		switch from {
+		case 'E':
+			{
+				GoNext('S', x, y-1)
+			}
+		case 'W':
+			{
+				GoNext('N', x, y+1)
+			}
+		case 'S':
+			{
+				GoNext('E', x-1, y)
+			}
+		case 'N':
+			{
+				GoNext('W', x+1, y)
+			}
+		}
+
+	} else if c == '/' {
+		switch from {
+		case 'W':
+			{
+				GoNext('S', x, y-1)
+			}
+		case 'E':
+			{
+				GoNext('N', x, y+1)
+			}
+		case 'N':
+			{
+				GoNext('E', x-1, y)
+			}
+		case 'S':
+			{
+				GoNext('W', x+1, y)
+			}
+		}
+
+	} else if c == '-' {
+		GoNext('E', x-1, y)
+		GoNext('W', x+1, y)
+	} else if c == '|' {
+		GoNext('N', x, y+1)
+		GoNext('S', x, y-1)
+	}
 }
 
 func Day16Part2() int {
 	input := fileparsers.ReadLines("inputs2023\\day16.txt")
 
-	return len(input)
+	parseDay16(input)
+
+	maxc := 0
+	for i := 0; i < sizeX16; i++ {
+
+		paths = make(map[string]int, 0)
+		uniquepaths = make(map[string]int, 0)
+
+		GoNext('N', i, 0)
+
+		pathCounts := len(paths)
+		maxc = max(maxc, pathCounts)
+	}
+
+	for i := 0; i < sizeX16; i++ {
+
+		paths = make(map[string]int, 0)
+		uniquepaths = make(map[string]int, 0)
+
+		GoNext('S', i, sizeY16-1)
+
+		pathCounts := len(paths)
+		maxc = max(maxc, pathCounts)
+	}
+
+	for i := 0; i < sizeY16; i++ {
+
+		paths = make(map[string]int, 0)
+		uniquepaths = make(map[string]int, 0)
+
+		GoNext('W', 0, i)
+
+		pathCounts := len(paths)
+		maxc = max(maxc, pathCounts)
+	}
+
+	for i := 0; i < sizeY16; i++ {
+
+		paths = make(map[string]int, 0)
+		uniquepaths = make(map[string]int, 0)
+
+		GoNext('E', sizeX16-1, i)
+
+		pathCounts := len(paths)
+		maxc = max(maxc, pathCounts)
+	}
+
+	return maxc
+}
+
+type NodeDay16 struct {
+	X     int
+	Y     int
+	Order int
+	From  *NodeDay16
+}
+
+func parseDay16(input []string) {
+	sizeX16 = len(input[0])
+	sizeY16 = len(input)
+	dataday16 = make([][]rune, 0)
+
+	for _, v := range input {
+		dataday16 = append(dataday16, []rune(v))
+	}
 }
--- main.go
+++ main.go
@@ -8,32 +8,6 @@ import (
 
 func main() {
 
-	// fmt.Println("Day 1 part 1: ", aoc2023.Day01Part1()) //53386
-	// fmt.Println("Day 1 part 2: ", aoc2023.Day01Part2()) //53312
-	// fmt.Println("Day 2 part 1: ", aoc2023.Day02Part1()) //2204
-	// fmt.Println("Day 2 part 2: ", aoc2023.Day02Part2()) //71036
-	// fmt.Println("Day 3 part 1: ", aoc2023.Day03Part1()) //512794
-	// fmt.Println("Day 3 part 2: ", aoc2023.Day03Part2()) //67779080
-	// fmt.Println("Day 4 part 1: ", aoc2023.Day04Part1()) //23235
-	// fmt.Println("Day 4 part 2: ", aoc2023.Day04Part2()) //5920640
-	// fmt.Println("Day 5 part 1: ", aoc2023.Day05Part1()) //486613012
-	// fmt.Println("Day 5 part 2: ", aoc2023.Day05Part2()) //56931769
-	// fmt.Println("Day 6 part 1: ", aoc2023.Day06Part1()) //2612736
-	// fmt.Println("Day 6 part 2: ", aoc2023.Day06Part2()) //29891250
-	// fmt.Println("Day 7 part 1: ", aoc2023.Day07Part1()) //248836197
-	// fmt.Println("Day 7 part 2: ", aoc2023.Day07Part2()) //251195607
-
-	// fmt.Println("Day 8 part 1: ", aoc2023.Day08Part1()) //
-	// fmt.Println("Day 8 part 2: ", aoc2023.Day08Part2()) //
-	// fmt.Println("Day 9 part 1: ", aoc2023.Day09Part1()) //
-	// fmt.Println("Day 9 part 2: ", aoc2023.Day09Part2()) //68
-	// fmt.Println("Day 10 part 1: ", aoc2023.Day10Part1()) // 69
-	// fmt.Println("Day 10 part 2: ", aoc2023.Day10Part2()) //
-	// fmt.Println("Day 11 part 1: ", aoc2023.Day11Part1())  //9918828
-	// fmt.Println("Day 11 part 2: ", aoc2023.Day11Part2())  //692506533832
-	//fmt.Println("Day 12 part 1: ", aoc2023.Day12Part1()) //
-	//fmt.Println("Day 112 part 2: ", aoc2023.Day12Part2()) //
-
-	fmt.Println("Day 14 part 1: ", aoc2023.Day14Part2()) //
+	fmt.Println("Day 16 part 1: ", aoc2023.Day16Part2()) //
 
 }
